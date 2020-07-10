0f693b9f728471bfe1b7aba1e6b66419ea7d0e75
diegofullstackjs <kelvin.fabiana@gmail.com>
2020-07-10 18:51:15 +0100

    Template initial react

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+function App() {
+  return <h1>Reactjs</h1>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    port: 4200,
+    open: true,
+    openPage: "index.html",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
